a531bd13a7bd66e67743be69582c60240a0f9b6b
takijemai <takijemai@gmail.com>
2023-04-09 19:52:13 +0200

    up

--- helpers/AuthHelpers.js
+++ helpers/AuthHelpers.js
@@ -6,33 +6,21 @@ const Token = require('../models/token');
 
 module.exports = {
   VerifyToken: async (req, res, next) => {
-   //let token = req.cookies.auth  ;
-   const token = await Token.findOne({ token: token}); 
-   
-console.log(token);
- // If token is not found in cookies, we search in MongoDB database
- if (!token) {
-  try {
-    //const token = await Token.findOne({ token: token}); 
-    //console.log(token);
-   
+   //let token = req.cookies.auth 
+   try {
+    const tokenData = await Token.findOne({ token: req.user.token }); 
+    if (tokenData) {
+      const token = tokenData.token;
+      const data = jwt.verify(token, dbConfig.secret);
+      req.user = data;
+      req.username = data.username;
+      next();
+    } else {
+      return res.status(HttpStatus.StatusCodes.FORBIDDEN).json({ message: 'No token provided' });
+    }
   } catch (err) {
-    return res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error retrieving token from database' });
+    return res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Failed to authenticate token' });
   }
-}
 
-// If token is still not found, return error
-if (!token) {
-  return res.status(HttpStatus.StatusCodes.FORBIDDEN).json({ message: 'No token provided' });
-}
-
-try {
-  const data = jwt.verify(token, dbConfig.secret);
-  req.user = data;
-  req.username = data.username;
-  next();
-} catch (err) {
-  return res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Failed to authenticate token' });
-}
   }
 }
