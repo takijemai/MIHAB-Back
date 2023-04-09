14e3d3a0b8bde4d13db3879eb4599d6e513248b6
takijemai <takijemai@gmail.com>
2023-04-09 19:35:58 +0200

    up

--- helpers/AuthHelpers.js
+++ helpers/AuthHelpers.js
@@ -6,35 +6,32 @@ const Token = require('../models/token');
 
 module.exports = {
   VerifyToken: async (req, res, next) => {
-   const token = req.cookies.auth  ;
-   
-//console.log(req.user.token);
- // If token is not found in cookies, we search in MongoDB database
- if (!token) {
-  try {
-    const tokenData = await Token.findOne({ token: req.user.token }); 
-    console.log(tokenData);
-    if (tokenData) {
-      token = tokenData.token;
+    let token = req.cookies.auth;
+  
+    // If token is not found in cookies, retrieve it from user object or other source
+    if (!token) {
+      // Assuming the token is stored in the user object as user.token
+      token = req.user.token;
+      // If token is still not found, return error
+      if (!token) {
+        return res.status(HttpStatus.StatusCodes.FORBIDDEN).json({ message: 'No token provided' });
+      }
     }
-  } catch (err) {
-    return res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error retrieving token from database' });
+  
+    try {
+      const tokenData = await Token.findOne({ token: token }); 
+      if (tokenData) {
+        const data = jwt.verify(tokenData.token, dbConfig.secret);
+        req.user = data;
+        req.username = data.username;
+        next();
+      } else {
+        return res.status(HttpStatus.StatusCodes.FORBIDDEN).json({ message: 'No token found' });
+      }
+    } catch (err) {
+      return res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error retrieving token from database' });
+    }
   }
-}
-
-// If token is still not found, return error
-if (!token) {
-  return res.status(HttpStatus.StatusCodes.FORBIDDEN).json({ message: 'No token provided' });
-}
-
-try {
-  const data = jwt.verify(token, dbConfig.secret);
-  req.user = data;
-  req.username = data.username;
-  next();
-} catch (err) {
-  return res.status(HttpStatus.StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Failed to authenticate token' });
-}
-  }
+  
   
 };
