0888935b7e55940f4659ff3fde54611dc9ef9db2
takijemai <takijemai@gmail.com>
2023-04-12 16:40:53 +0200

    up

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,6 @@
 module.exports= {
     url : 'mongodb+srv://chatapp:Ta50356686@cluster0.ciybmqk.mongodb.net/chatapp?retryWrites=true&w=majority',
-    secret: 'myjsonwentokensecret',
+    secret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2FmNDI2NDVhZTRlOGNiNzc5NWZiYTkiLCJ1c2VybmFtZSI6ImptYWsiLCJpYXQiOjE2ODEyNTY0MzgsImV4cCI6MTY4MTI3NDQzOH0.p6LY5wqOewu26JexxQiD7TKNifWpIktXtlubykNDtYw',
     apikey:'i8bcbphlou1ueo9b8ahue932h5s6yo7d',
     apikeycloudinary:'884673834196555',
 
